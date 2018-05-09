82aac847864fa0c5e68771c4ac61956f18d16ee7
Unknown <ifavilla@gmail.com>
2018-05-09 14:07:25 -0300

    Inicio do projeto. Espero q de certo

---
+++ src/app/app.module.ts
@@ -0,0 +1,63 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+//AngularFire
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireOfflineModule } from 'angularfire2-offline';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAr6KTkO0XlkVhMpRsYUv0cY0w37MH0Ez8",
+  authDomain: "spoleto-71c07.firebaseapp.com",
+  databaseURL: "https://spoleto-71c07.firebaseio.com",
+  projectId: "spoleto-71c07",
+  storageBucket: "spoleto-71c07.appspot.com",
+  messagingSenderId: "1017161849787"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireOfflineModule,
+    AngularFireDatabase,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule { }
